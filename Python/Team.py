695bbef9ddb9ab84e699c4e3a54ed65bbb0056a2
nguyengiakhang646 <nguyengiakhang646@gmail.com>
2024-03-14 22:59:25 +0700

    Refactor team generation and printing in Team.py

--- Python/Team.py
+++ Python/Team.py
@@ -12,15 +12,15 @@ while True:
 	random.shuffle(players)
 	for i in range(0, n):
 		team.append(players[len(players) // n * i: len(players) // n * (i + 1)])
-		print("Team " + str(i + 1) + ":", end = " ")
+		print("Team " + str(i + 1) + ": ", end="")
 		for j in range(0, len(team[i])):
 			if j == 0:
-				print(team[i][j] + "(C)", end = ", ")
+				print(team[i][j] + "(C), ", end="")
 			else:
 				if j == len(team[i]) - 1:
 					print(team[i][j])
 				else:
-					print(team[i][j], end = ", ")
+					print(team[i][j] + ", ", end="")
 	response = input("\nPick teams again? Type y or n: ")
 	if response == "n":
 		os.system("cls")
